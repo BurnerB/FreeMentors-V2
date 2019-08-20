0e4183ed28878f74bf0cfa2ac47f2b97c9cf2310
BurnerB <barnabykamau@gmail.com>
2019-08-20 10:25:35 +0300

    ft(api):User signup[Finish #167962035]

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import Users from '../controllers/user';
+import Validations from '../middleware/userValidation';
+
+const router = express.Router();
+
+
+router.post('/auth/signup',Validations.validateSignup, Users.registerUser);
+
+
+export default router;
