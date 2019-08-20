622094350cc5ae3f98b94e54c4f6cf6480b856a2
BurnerB <barnabykamau@gmail.com>
2019-08-20 14:03:49 +0300

    ch(api):Failing signup tests[#167962070]

--- server/middleware/userValidation.js
+++ server/middleware/userValidation.js
@@ -5,18 +5,21 @@ class Validations {
   static async validateSignup(req, res, next) {
     try {
       const schema = {
-        firstName: Joi.string().trim().min(3).max(15).regex(/^[a-zA-Z]*$/)
+        firstName: Joi.string().trim().min(3).max(15)
+          .regex(/^[a-zA-Z]*$/)
           .required()
           .error(() => 'Firstname is a required field with a min of 3 chars and no special chars or numbers'),
 
-        lastName: Joi.string().trim().min(3).max(15).regex(/^[a-zA-Z]*$/)
+        lastName: Joi.string().trim().min(3).max(15)
+          .regex(/^[a-zA-Z]*$/)
           .required()
           .error(() => 'Lastname is a required field with a min of 3 chars and no special chars or numbers'),
 
         email: Joi.string().trim().email({ minDomainSegments: 2 }).required()
           .error(() => 'Email is a required field and must be valid'),
 
-        password: Joi.string().trim().min(5).max(15).alphanum()
+        password: Joi.string().trim().min(5).max(15)
+          .alphanum()
           .required()
           .error(() => 'Password is a required field with a min of 5 chars and no special chars'),
 
@@ -25,16 +28,18 @@ class Validations {
           .error(() => 'Address is a required field with a min of 5 chars and no special chars'),
 
         bio: Joi.string().trim().max(200)
-            .required()
-            .error(()=>'Bio is a required field with a maximum of 200 chars'),
-        
-        occupation: Joi.string().trim().min(3).max(15).alphanum()
-            .required()
-            .error(()=>'Occupation is a required field with minimum of 3 chars maximum of 15 chars'),
-        
-        expertise: Joi.string().trim().min(3).max(15).alphanum()
-            .required()
-            .error(()=>'Expertise is a required field with a minimum of 3 chars maximum of 15 chars'),
+          .required()
+          .error(() => 'Bio is a required field with a maximum of 200 chars'),
+
+        occupation: Joi.string().trim().min(3).max(15)
+          .alphanum()
+          .required()
+          .error(() => 'Occupation is a required field with minimum of 3 chars maximum of 15 chars'),
+
+        expertise: Joi.string().trim().min(3).max(15)
+          .alphanum()
+          .required()
+          .error(() => 'Expertise is a required field with a minimum of 3 chars maximum of 15 chars'),
       };
       const { error } = Joi.validate(req.body, schema);
 
