f2c1d5e986a9252382be7e26226135e9dc575dbd
BurnerB <barnabykamau@gmail.com>
2019-08-20 10:24:17 +0300

    ft(api):User signup[#167962035]

---
+++ server/helpers/responses.js
@@ -0,0 +1,48 @@
+class Responses {
+    static authsuccess(statusCode, message,data, res) {
+      res.status(statusCode).json({
+        status: statusCode,
+        message:message,
+        data,
+      });
+    }
+  
+    static handleSuccess(statusCode, message,data, res) {
+      res.status(statusCode).json({
+        status: statusCode ,
+        message:message,
+        data,
+      });
+    }
+  
+    static success(statusCode, message, res) {
+      res.status(statusCode).json({
+        status: statusCode,
+        data: message,
+      });
+    }
+  
+    static handleError(statusCode, message, res) {
+      res.status(statusCode).json({
+        status: statusCode,
+        error: message,
+      });
+    }
+  
+    static validationError(statusCode, data, res) {
+      res.status(statusCode).json({
+        status: statusCode,
+        error: data,
+      });
+    }
+  
+    static catchError(statusCode, data, res) {
+      res.status(statusCode).json({
+        status: statusCode,
+        error: data,
+      });
+    }
+  
+  }
+  
+  export default Responses;
