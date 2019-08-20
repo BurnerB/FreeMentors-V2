abc5189f8d27c276b7bc5d0ec62ef6b047655013
BurnerB <barnabykamau@gmail.com>
2019-08-20 10:24:24 +0300

    ft(api):User signup[#167962035]

---
+++ server/helpers/tokenGen.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+class Token {
+  static genToken(payload) {
+    const token = jwt.sign({payload}, process.env.JWT_KEY, {
+      expiresIn: '100d',
+    });
+    return token;
+  }
+}
+
+export default Token;
