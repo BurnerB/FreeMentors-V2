307dc25ec998affea2a09f35eed9f48884338eb2
BurnerB <barnabykamau@gmail.com>
2019-08-21 22:37:08 +0300

    ft(api):Admin make User Mentor[#168005740]

--- server/tests/admin.test.js
+++ server/tests/admin.test.js
@@ -1,7 +1,10 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 import app from '../../app';
+
+dotenv.config();
 
 const { expect } = chai;
 chai.should();
@@ -63,59 +66,60 @@ describe('ADMIN', () => {
     process.env.JWT_KEY, {
       expiresIn: '100d',
     });
+    done();
+  });
 
-    describe('/patch user to mentor', () => {
-      it('Admin should successfully make user into mentor', (done) => {
-        chai.request(app)
-          .patch('/api/v1/user/1')
-          .set('authorization', `Bearer ${adminToken}`)
-          .end((err, res) => {
-            res.should.have.status(200);
-            expect(res).to.be.an('object');
-            expect(res.body.message).equals('User account changed to mentor');
-            if (err) return done();
-            done();
-          });
-      });
+  describe('/patch user to mentor', () => {
+    it('Admin should successfully make user into mentor', (done) => {
+      chai.request(app)
+        .patch('/api/v1/user/1')
+        .set('authorization', `Bearer ${adminToken}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          expect(res).to.be.an('object');
+          expect(res.body.data.message).equals('User account changed to mentor');
+          if (err) return done();
+          done();
+        });
+    });
 
-      it("Mentor should'nt successfully make user into mentor", (done) => {
-        chai.request(app)
-          .patch('/api/v1/user/1')
-          .set('authorization', `Bearer ${mentorToken}`)
-          .end((err, res) => {
-            res.should.have.status(403);
-            expect(res).to.be.an('object');
-            expect(res.body.error).equals('Forbidden');
-            if (err) return done();
-            done();
-          });
-      });
+    it("Mentor should'nt successfully make user into mentor", (done) => {
+      chai.request(app)
+        .patch('/api/v1/user/1')
+        .set('authorization', `Bearer ${mentorToken}`)
+        .end((err, res) => {
+          res.should.have.status(403);
+          expect(res).to.be.an('object');
+          expect(res.body.error).equals('ACCESS DENIED! Not an Admin');
+          if (err) return done();
+          done();
+        });
+    });
 
-      it("User should'nt successfully make user into mentor", (done) => {
-        chai.request(app)
-          .patch('/api/v1/user/1')
-          .set('authorization', `Bearer ${userToken}`)
-          .end((err, res) => {
-            res.should.have.status(403);
-            expect(res).to.be.an('object');
-            expect(res.body.error).equals('Forbidden');
-            if (err) return done();
-            done();
-          });
-      });
+    it("User should'nt successfully make user into mentor", (done) => {
+      chai.request(app)
+        .patch('/api/v1/user/1')
+        .set('authorization', `Bearer ${userToken}`)
+        .end((err, res) => {
+          res.should.have.status(403);
+          expect(res).to.be.an('object');
+          expect(res.body.error).equals('ACCESS DENIED! Not an Admin');
+          if (err) return done();
+          done();
+        });
+    });
 
-      it('Token is required make user into mentor', (done) => {
-        chai.request(app)
-          .patch('/api/v1/user/1')
-          .set('authorization', ' ')
-          .end((err, res) => {
-            res.should.have.status(401);
-            expect(res).to.be.an('object');
-            expect(res.body.error).equals('Unauthorized access');
-            if (err) return done();
-            done();
-          });
-      });
+    it('Token is required make user into mentor', (done) => {
+      chai.request(app)
+        .patch('/api/v1/user/1')
+        .set('authorization', ' ')
+        .end((err, res) => {
+          res.should.have.status(401);
+          expect(res).to.be.an('object');
+          expect(res.body.error).equals('ACCESS DENIED! No token provided');
+          if (err) return done();
+          done();
+        });
     });
   });
 });
