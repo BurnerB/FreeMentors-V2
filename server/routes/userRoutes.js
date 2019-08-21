eaabb94cded75e5dfc4a216d526ae7c096a46660
BurnerB <barnabykamau@gmail.com>
2019-08-21 23:37:35 +0300

    ft(api):all mentors[#168005910]

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Users from '../controllers/user';
+import Mentors from '../controllers/mentor';
 import Validations from '../middleware/userValidation';
 
 const router = express.Router();
@@ -7,6 +8,7 @@ const router = express.Router();
 
 router.post('/auth/signup', Validations.validateSignup, Users.registerUser);
 router.post('/auth/signin', Validations.validateLogin, Users.userLogin);
+router.get('/mentors', Mentors.getAllMentors);
 
 
 export default router;
