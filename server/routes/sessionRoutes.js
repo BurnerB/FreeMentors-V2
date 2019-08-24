4fb8e43a286b16da86181fd0e006bbdb9487426f
BurnerB <barnabykamau@gmail.com>
2019-08-24 10:09:49 +0300

    ft(api): accept/reject session[Finish #168073889]

--- server/routes/sessionRoutes.js
+++ server/routes/sessionRoutes.js
@@ -2,11 +2,14 @@ import express from 'express';
 import Sessions from '../controllers/session';
 import Validations from '../middleware/sessionValidation';
 import authToken from '../middleware/authToken';
+import Mentor from '../middleware/mentor';
 
 
 const router = express.Router();
 
 router.post('/sessions', authToken, Validations.validateSessions, Sessions.requestSession);
+router.patch('/sessions/:sessionId/accept', [authToken, Mentor], Sessions.chooseSession);
+router.patch('/sessions/:sessionId/reject', [authToken, Mentor], Sessions.chooseSession);
 
 
 export default router;
